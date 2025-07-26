38dec42c4f6397d8dffd5441089e9843cbbcaa2d
Satya <satyanarayanamylavarapuaws@gmail.com>
2025-07-26 12:06:58 +0530

    fix

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,6 +1,6 @@
 #vpc
 resource "aws_vpc" "stage-vpc" {
-  cidr_block           = "10.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
