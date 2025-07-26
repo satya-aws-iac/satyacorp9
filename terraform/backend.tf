ccae7c558f530dbd7d666e73be49c094f5548879
Satya <satyanarayanamylavarapuaws@gmail.com>
2025-07-26 12:17:23 +0530

    s32

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,47 +1,18 @@
-# versions.tf (or main.tf)
-terraform {
-  backend "s3" {
-    bucket         = "s3-mylavarsatyacorp-state-bucket"
-    key            = "global/mystatefile/terraform.tfstate"
-    region         = "eu-west-2"
-    dynamodb_table = "tfstate-lock-table-satya"
-    encrypt        = true
-  }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
-
-variable "aws_region" {
-  description = "The AWS region to deploy resources into."
-  type        = string
-  default     = "eu-west-2" # Ensure this matches your S3 backend region
-}
-
-# s3.tf
+#create  s3 bucket
 resource "aws_s3_bucket" "tfstatebucket-satya" {
   bucket = "s3-mylavarsatyacorp-state-bucket"
-
-  # It's good practice to add tags for organization
-  tags = {
-    Name        = "Terraform State Bucket"
-    Environment = "Production"
-  }
+  region = "eu-west-2"
 }
 
-# dynamodb.tf
+
+
+
+# Create DynamoDB table for state locking
 resource "aws_dynamodb_table" "tfstate-lock-table-satya" {
-  name                        = "tfstate-lock-table-satya"
-  billing_mode                = "PAY_PER_REQUEST"
-  hash_key                    = "LockID"
+  name         = "tfstate-lock-table-satya"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
   deletion_protection_enabled = true
 
   attribute {
