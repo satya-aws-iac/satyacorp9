39227d20f56c7a8ceed5d32b34dc0efedd8a668c
Satya <satyanarayanamylavarapuaws@gmail.com>
2025-07-26 13:39:29 +0530

    update

--- terraform/public-routes.tf
+++ terraform/public-routes.tf
@@ -2,7 +2,7 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.stage-vpc.id
 
   route {
-    cidr_block = "0.0.0/0"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
   tags = {
