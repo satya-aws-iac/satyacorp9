3a3118c702365ba093043c1cc8d4bd2868c07c20
Satya <satyanarayanamylavarapuaws@gmail.com>
2025-07-26 12:10:20 +0530

    fix-1

--- terraform/db-subnets.tf
+++ terraform/db-subnets.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "db-subnet" {
   vpc_id            = aws_vpc.stage-vpc.id
-  cidr_block        = "10.0.128.0./19"
+  cidr_block        = "10.0.128.0/19"
   availability_zone = "eu-west-2a"
   tags = {
     Name = "stage-db-subnet-eu-west-2a"
