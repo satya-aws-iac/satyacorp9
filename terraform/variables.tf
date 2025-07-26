8fd75e66255a8a022629f895a2b9c0b15eabf136
msatya <mllsatyanarayana@gmail.com>
2025-07-26 09:13:27 +0530

    cleanup

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,48 +1,6 @@
-variable "namespace" {
-  description = "Namespace for the resources"
-  type        = string
-
-}
-
-
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "eu-west-2"
+
 }
 
-variable "environment" {
-  description = "Environment name"
-  type        = string
-}
-
-variable "environment" {
-  description = "Environment name"
-  type        = string
-}
-
-variable "vpc_cidr" {
-  description = "CIDR block for VPC"
-  type        = string
-}
-
-variable "availability_zones" {
-  description = "List of availability zones"
-  type        = list(string)
-}
-
-variable "private_subnet_cidrs" {
-  description = "CIDR blocks for private subnets"
-  type        = list(string)
-}
-
-variable "public_subnet_cidrs" {
-  description = "CIDR blocks for public subnets"
-  type        = list(string)
-}
-
-variable "tags" {
-  description = "Tags to apply to resources"
-  type        = map(string)
-  default     = {}
-}
